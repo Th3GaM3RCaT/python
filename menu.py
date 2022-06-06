50806a0bd17da99c9a10d35545eeff8e1024043f
Th3GaM3RCaT <rodrigochanqueyo@gmail.com>
2022-06-06 11:57:49 -0400

    menu con try y otros cambios

--- menu.py
+++ menu.py
@@ -11,26 +11,29 @@ while option !=0:
     print ("4 ") #(mas o menos puntos)
     print ("5 ")
     print ("0 salir")
-    
-    option = int (input ())
-    a = 0
-    b = 0
-    if option == 1: #1 
-        print ("opcion 1")
-        time.sleep (1)
+    try:
+        option = int (input ())
+        a = 0
+        b = 0
+        if option == 1: #1 
+            print ("opcion 1")
+            print (f"a es {a} ")
+            time.sleep (1)
 
-    elif option == 2: #2 
-        print ("opcion 2")
+        elif option == 2: #2 
+            print ("opcion 2")
 
-    elif option == 3: #3 
-        print ("opcion 3")
+        elif option == 3: #3 
+            print ("opcion 3")
 
-    elif option == 4: #4 
-        print ("opcion 4")
+        elif option == 4: #4 
+            print ("opcion 4")
 
-    elif option == 5: #5 
-        print ("opcion 5")
+        elif option == 5: #5 
+            print ("opcion 5")
 
-    elif option == 0: #0 salir
-        print ("saliendo ...")
+        elif option == 0: #0 salir
+            print ("saliendo ...")
+    except ValueError:
+        print ()
 
